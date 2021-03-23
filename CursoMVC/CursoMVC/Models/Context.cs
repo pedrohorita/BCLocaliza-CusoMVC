5a26d0ecb3f5c12830c7d9f456ab60f57abb3983
Pedro R T A Horita <pedro_horita@hotmail.com>
2021-03-23 16:35:08 -0300

    Criação de Produtos
    Criação do crud de produtos e configuração de label e range.

--- CursoMVC/CursoMVC/Models/Context.cs
+++ CursoMVC/CursoMVC/Models/Context.cs
@@ -9,6 +9,7 @@ namespace CursoMVC.Models
     public class Context : DbContext
     {
         public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Produto> Produto { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(connectionString:@"Server=(localdb)\mssqllocaldb;Database=Cursomcv;Integrated Security=True");
