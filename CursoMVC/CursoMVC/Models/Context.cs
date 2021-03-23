000c9962e288a2da19c27618ad83a056ce4c87cb
Pedro R T A Horita <pedro_horita@hotmail.com>
2021-03-23 20:42:36 -0300

    Implementação de testes
    criação dos testes de funcionamento básico do crud de categoria.

--- CursoMVC/CursoMVC/Models/Context.cs
+++ CursoMVC/CursoMVC/Models/Context.cs
@@ -8,11 +8,16 @@ namespace CursoMVC.Models
 {
     public class Context : DbContext
     {
-        public DbSet<Categoria> Categorias { get; set; }
-        public DbSet<Produto> Produtos { get; set; }
+        public virtual DbSet<Categoria> Categorias { get; set; }
+        public virtual DbSet<Produto> Produtos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(connectionString:@"Server=(localdb)\mssqllocaldb;Database=Cursomcv;Integrated Security=True");
+        }
+
+        public virtual void SetModified(object entity)
+        {
+            Entry(entity).State = EntityState.Modified;
         }
     }
 }
